932c7cd49467ca7db1f981f3ee74acd9f43c5b2c
dariusbrinzan <brinzandarius27@gmail.com>
2024-01-02 17:55:22 +0200

    Started backend

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+import { revalidatePath } from "next/cache";
+
+export async function updateUser(userId: string, username: string, name: string, image: string, path: string, bio: string): Promise<void> {
+    connectToDB();
+
+    try {
+        await User.findOneAndUpdate(
+            { id: userId },
+            { 
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true
+            },
+            { upsert: true }
+        );
+    
+        if (path === '/profile/edit') {
+            revalidatePath(path);
+        }
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`);
+    }
+}
