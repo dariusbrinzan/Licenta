11eccadd23f87ffbc938de8e23fe343e364347df
dariusbrinzan <brinzandarius27@gmail.com>
2024-01-11 21:57:12 +0200

    Added icon and description

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,8 +13,8 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  title: "StudZone",
+  description: "A social media platform for students",
 };
 
 export default function RootLayout({
