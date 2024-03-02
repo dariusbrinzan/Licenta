d4e98b007261fabc992ba3267ba01301cfff2705
dariusbrinzan <brinzandarius27@gmail.com>
2024-03-02 16:20:49 +0200

    starting chat

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -47,6 +47,22 @@ const LeftSidebar = () => {
           <SignOutButton signOutCallback={() => router.push("/sign-in")}>
             <div className='flex cursor-pointer gap-4 p-4'>
               <Image
+                src='/assets/chat.svg'
+                alt='Chat'
+                width={24}
+                height={24}
+              />
+              <p className='text-light-2 max-lg:hidden'>Chat</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+
+      <div className='mt-10 px-6'>
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className='flex cursor-pointer gap-4 p-4'>
+              <Image
                 src='/assets/logout.svg'
                 alt='logout'
                 width={24}
